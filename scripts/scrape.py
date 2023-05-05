252538ed1e412b8c7aa0071e6552164b7e947b73
Euvaz <ee470189-bbb3-46d9-923e-0db31876ec86@aleeas.com>
2023-05-05 13:55:22 -0500

    feat: Add basic scraping

--- scripts/scrape.py
+++ scripts/scrape.py
@@ -1,20 +1,34 @@
 import sqlite3
+import httplib2
+import json
 
 # Initilize Connection and Cursor
 connection = sqlite3.connect("locked.sqlite")
 cursor = connection.cursor()
 
-# Read from github table
-response = cursor.execute("SELECT * FROM github")
-print(response.fetchmany())
+# Initialize HTTP interface
+http = httplib2.Http()
 
-# Read from github_repositories table
-response = cursor.execute("SELECT * FROM github_repositories")
-print(response.fetchmany())
+GITHUB_USER = "https://api.github.com/users/Euvaz"
+GITHUB_REPO_URL = GITHUB_USER + "/repos"
 
-# Read from github_stars table
-response = cursor.execute("SELECT * FROM github_stars")
-print(response.fetchmany())
+# Get repository information
+gh_repo_payload = http.request(GITHUB_REPO_URL, method="GET")[1]
+gh_repo_content = json.loads(gh_repo_payload.decode())
+
+gh_repo_owners = []
+gh_repo_names = []
+for repo in gh_repo_content:
+    gh_repo_owners.append(repo["owner"]["login"])
+    gh_repo_names.append(repo["name"])
+
+gh_repo_complete = list(map(lambda x, y:(x,y), gh_repo_owners, gh_repo_names))
+for repo in gh_repo_complete:
+    cursor.execute(f"""INSERT INTO github_repositories (owner, name)
+                   VALUES ("{repo[0]}", "{repo[1]}")""")
+
+# Commit changes
+connection.commit()
 
 # Close Connection
 connection.close()
